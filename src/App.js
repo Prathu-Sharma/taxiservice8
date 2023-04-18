f1a891b219c74c254b767aeab5d20aff4bec799b
Pritish <growcontent03@gmail.com>
2023-04-18 22:37:08 +0530

    Changes in the code

--- src/App.js
+++ src/App.js
@@ -2,29 +2,9 @@ import './App.css';
 import Header from './Components/Header';
 
 function App() {
-<<<<<<< HEAD
   return(
 
   <Header/>
-=======
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacts
-        </a>
-      </header>
-    </div>
->>>>>>> 62a256f172943dedef82afb6c0991d6a3e155814
   );
 }
 
--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Button.css";
 function Button() {
   return (
-<button className="btn">Book a Taxi</button>
+<button className="btn">Book an Taxi</button>
   );
 }
 
