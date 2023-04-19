cc538405336e231ba11e9e14abc5f62c07a847fd
Pritish <growcontent03@gmail.com>
2023-04-19 21:44:56 +0530

    Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Form from './Components/Form';
 import Header from './Components/Header';
 
 function App() {
   return(
-
-  <Header/>
+      <>
+        <Header/>
+        <Form />
+      </>
   );
 }
 
