2f7597ffc714d5110d7bd76f209d476e49632817
prathu sharma <prathusharma88884@gmail.com>
2023-05-06 10:58:05 +0530

    changes has been done in card section

--- src/Components/Places.js
+++ src/Components/Places.js
@@ -6,7 +6,7 @@ import './Places.css';
 const Places = () =>{
  return(
   <div>
-    <div className='Heading8'>Places To Visit</div>
+    <div className='Heading8'><span style={{color:"black"}}>Places</span> To Visit</div>
     <div className='Container1'>
      <Card className="box1"  imagess="https://indiathrills.com/wp-content/uploads/2021/07/2017-11-21-768x431.jpg" city="Rishikesh" item1="Neer Waterfall" item2="Laxman Jhula" item3="Ram Jhula" item4="Janki Setu"/>
      <Card  className="box1"imagess="https://www.jagranimages.com/images/newimg/12112022/12_11_2022-dehradun_top_news_23199140.webp" city="Dehradun" item1="FRI" item2="Tapkeshwar Mahadev" item3="Buddha Temple" item4="Robbers Cave"/>
